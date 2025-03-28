0423b31516383cee438a91ced50f6689904b4f82
AmashH <amash4747@gmail.com>
2025-03-28 04:43:17 +0000

    correct app name and structure

--- webapp/ebooklicensing/asgi.py
+++ webapp/ebooklicensing/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for blockbooks project.
+ASGI config for ebooklicensing project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blockbooks.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ebooklicensing.settings')
 
 application = get_asgi_application()
