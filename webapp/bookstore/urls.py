fabe76a39510ac109a1086d76778085f1f654e65
AmashH <amash4747@gmail.com>
2025-04-16 15:29:44 +0100

    delete old django web app,

--- webapp/bookstore/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.shop, name='shop'),  # Shop page
-    path('buy/<str:book_id>/', views.buy_book, name='buy_book'),  # Buy a book
-    path('profile/', views.profile, name='profile'),  # Profile page
-]
