d06dcee6b1c5729e0402262188e930a1af93f953
AmashH <amash4747@gmail.com>
2025-04-22 12:36:32 +0100

    comment out fakelicense model, update licensesviewspy mylicenses to connect to blockchain

--- webapp/ebooklicensing/licenses/admin.py
+++ webapp/ebooklicensing/licenses/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import FakeLicense
 
-admin.site.register(FakeLicense)
+#admin.site.register(FakeLicense)
 
 # Register your models here.
--- webapp/ebooklicensing/licenses/views.py
+++ webapp/ebooklicensing/licenses/views.py
@@ -4,11 +4,23 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import FakeLicense
 from django.contrib.auth.decorators import login_required
+import requests 
 
 @login_required
 def my_licenses(request):
-    licenses = FakeLicense.objects.filter(owner=request.user)
-    return render(request, 'my_licenses.html', {'licenses': licenses})
+    user_id=request.user.username # .username is used as ownerid in chaincode
+    
+    known_license_ids= ['LIC001', 'LIC002'] #licenses made with manual invokes on test network to start
+    
+    licenses = [] 
+    for license_id in known_license_ids:
+        res = requests.get(f"http://localhost:8080/api/licenses/{license_id}")  #acess api controller endpoint
+        if res.ok(): #if api connection succeeds
+            data = res.json().get("data") #pulls actual license JSON returned
+            if data:
+                licenses.append(data) #adds to list so that it can be passed to the template
+                
+    return render(request, "my_licenses.html", {"licenses": licenses})            
 
 @login_required
 def transfer_license_view(request):
@@ -22,7 +34,6 @@ def transfer_license_view(request):
         # Placeholder logic for now
         message = f"Simulated transfer of license {license_id} to user {new_owner}."
 
-    return render(request, 'transfer_license.html', {
+    return render(request, 'transfer_licenses.html', {
         'licenses': licenses,
-        'message': message
-    })
+        'message': message})
